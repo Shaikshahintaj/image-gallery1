1d3db717edc9747c67dc1d753e16c1fff3f841b6
Shaikshahintaj <133940555+Shaikshahintaj@users.noreply.github.com>
2024-03-31 10:29:05 +0000

    new users added

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return <div>
     <h1>Users</h1>
-
     <section className="container">
-    <User name="sha" job="developer Location="hyd/>
-    <User job="software support" Location="vij" />
+    <User name="sha" Job="developer" Location="hyd"/>
+    <User Job="software developer" Location="vij" />
     <User name="taj" Location="ongole"/>
+    <User name="Zakeer" job="UI developer" Location="ongole"/>
+    <User name="Jabeen" job="UI design" Location="vijayawada"/>
+
     </section>
     </div>
 }
 
 
 function User(props){
-  return <div className="user">
+  return <div className="user other-class">
     <h3>{props.name}</h3>
     <p>Job:{props.Job}</p>
     <p>Location:{props.Location}</p>
